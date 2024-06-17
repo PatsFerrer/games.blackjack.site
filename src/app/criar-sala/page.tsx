c12df45363dedfa3cac2b1e1de906a185effaaa3
Patricia Ferrer <patricia.ferreira@esx.com.br>
2024-06-17 10:39:38 -0300

    chore: Configuração de rotas

---
+++ src/app/criar-sala/page.tsx
@@ -0,0 +1,5 @@
+export default function CriarSala() {
+  return (
+    <div>Criar Mesa</div>
+  )
+}
---
+++ src/app/editar-cadastro/page.tsx
@@ -0,0 +1,5 @@
+export default function EditarCadastro() {
+  return (
+    <div>Editar Cadastro</div>
+  )
+}
