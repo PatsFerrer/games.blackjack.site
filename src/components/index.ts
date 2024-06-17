bd4aef6ed18690670d3adfbd8a09e9837eee462b
Patricia Ferrer <patricia.ferreira@esx.com.br>
2024-06-17 10:44:42 -0300

    feat: index dos components

---
+++ src/components/index.ts
@@ -0,0 +1,11 @@
+//import Jogador from '@/components/Jogador';
+//import Botao from './Botao';
+import Navbar from './Navbar';
+//import Dealer from './Dealer';
+
+export {
+  //Jogador,
+  //Botao,
+  Navbar,
+  //Dealer
+}
