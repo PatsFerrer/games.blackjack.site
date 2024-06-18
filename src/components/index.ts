838c2dc70eea87fcf4b816bee6ef2e9ee8d30c2f
Aira Arima <aira.arima@esx.com.br>
2024-06-18 08:12:36 -0300

    feat: tela para listar telas disponíveis

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,10 +2,14 @@
 //import Botao from './Botao';
 import Navbar from './Navbar';
 //import Dealer from './Dealer';
+import Salas from './Salas';
+import SalaForm from './SalaForm';
 
 export {
   //Jogador,
   //Botao,
   Navbar,
   //Dealer
+  Salas,
+  SalaForm
 }
