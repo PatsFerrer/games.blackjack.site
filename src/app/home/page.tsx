f72c8a63cbab5548a946aacc28f6e8068c433b96
Patricia Ferrer <patricia.ferreira@esx.com.br>
2024-06-20 16:36:13 -0300

    docs: adicionar página de exemplo para autenticação e navegação
    - Página que exemplifica o uso de getServerSession para autenticação
    
    - Redirecionamento para a página inicial se não houver sessão
    
    - Exibição de e-mail do usuário autenticado
    
    - Inclusão do botão de logout e componente ServerFunction

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,5 +1,27 @@
-export default function PaginaInicial() {
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+
+import { LogoutButton } from "@/components";
+import ServerFunction from "@/components/ServerFunction";
+
+export default async function PaginaInicial() {
+
+  const session = await getServerSession();
+
+  if (!session) {
+    redirect('/')
+  }
+
   return (
-    <div>Página inicial</div>
+    <div className="mt-5">Página inicial
+      <h1>Olá,{session?.user?.email} </h1>
+      <div>
+        <LogoutButton />
+      </div>
+      <div>
+        {/* acessando o Function1 */}
+        <ServerFunction />
+      </div>
+    </div>
   )
 }
