cb7dc98ac58235b83946d044d7b9598f3940211c
Patricia Ferrer <patricia.ferreira@esx.com.br>
2024-06-20 16:18:33 -0300

    feat: configurar fetchServer

---
+++ src/libs/fetchServer/index.ts
@@ -0,0 +1,24 @@
+import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
+
+export const fetchServer = async (
+  input: string | URL | Request,
+  init?: RequestInit | undefined
+): Promise<Response> => {
+
+  const token = cookies().get('token');
+
+  const response = await fetch(input, {
+    ...init,
+    headers: {
+      ...init?.headers,
+      ...(token && { Authorization: `Bearer ${token.value}` }),
+    },
+  });
+
+  if (response.status === 401) {
+    return redirect('/');
+  }
+
+  return response;
+}
