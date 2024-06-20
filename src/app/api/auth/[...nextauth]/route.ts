ed8b043b24b7a7f642c44464c173fe6c30966876
Patricia Ferrer <patricia.ferreira@esx.com.br>
2024-06-20 16:16:17 -0300

    feat: configurar route do NextAuth

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,67 @@
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { cookies } from "next/headers";
+
+const handler = NextAuth({
+  pages: {
+    signIn: '/',
+  },
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        login: {},
+        senha: {}
+      },
+      async authorize(credentials, req) {
+        if (!credentials) {
+          return null;
+        }
+
+        try {
+          const response = await fetch('http://localhost:7071/api/auth/login', {
+            method: 'POST',
+            body: JSON.stringify({
+              login: credentials.login, //back usa login e nao email
+              senha: credentials.senha,
+            }),
+            headers: { "Content-Type": "application/json" }
+          });
+
+          if (response.status !== 200) return null;
+
+          const authData = await response.json();
+
+          if (!authData.token || !authData.usuario) return null;
+
+          cookies().set('token', authData.token);
+
+          return {
+            id: authData.usuario.id,
+            login: authData.usuario.login,
+            email: authData.usuario.email,
+          }
+
+        } catch (error) {
+          return null;
+        }
+      }
+    })
+  ],
+
+  // pra aparecer o nome do nosso usuario, tem que configurar o callback
+  
+  // callbacks: {
+
+  //   async jwt({ token, user, account, profile }) {
+  //     console.log('token', token)
+
+  //     return token
+  //   }
+
+  // }
+
+
+})
+
+export { handler as GET, handler as POST }
