90d90a4f615a362a078eb8c34c33d98b2f0ab03f
Patricia Ferrer <patricia.ferreira@esx.com.br>
2024-06-21 19:10:46 -0300

    refactor: mover três páginas para a pasta auth

--- src/app/(auth)/home/page.tsx
+++ src/app/(auth)/home/page.tsx
@@ -8,20 +8,25 @@ import { cookies } from "next/headers";
 export default async function PaginaInicial() {
 
   const session = await getServerSession();
-  
+
   const user = JSON.parse(cookies().get('user')!.value)
 
-  const {login, avatar, email } = user;
+  const { login, avatar, email } = user;
 
   if (!session) {
     redirect('/')
   }
 
   return (
-    <div className="mt-5">Página inicial
+    <div className="">Página inicial
+      <h1 className="text-2xl">Todas as mesas</h1>
       <h1>Olá, {login} </h1>
       <h1>email: {email} </h1>
-      <img src={avatar} alt={`avatar de ${login}`} />
+
+      <div className="w-10 rounded-full ">
+        <img src={avatar} alt={`avatar de ${login}`} />
+      </div>
+
       <div>
         <LogoutButton />
       </div>
