b49dc43d72ddef83f164f137f01f7ea01976eef2
Patricia Ferrer <patricia.ferreira@esx.com.br>
2024-06-27 15:08:32 -0300

    refactor: atualizar exportações de componentes e adicionar novo arquivo de exportação

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,25 +1,27 @@
 import Jogador from './Jogador';
 import Navbar from './Navbar';
-// import SalaCard from './SalaCard';
-// import SalaForm from './SalaForm';
+import SalaForm from './SalaForm';
 import Dealer from './Dealer';
 import FormLogin from './FormLogin';
 import LogoutButton from './LogoutButton';
 import FormCadastro from './FormCadastro';
 import ListarPessoasButton from './ListarPessoasButton';
 import Loading from './Loading';
-// import Salas from './Salas';
+import Salas from './Salas';
+import HomeClient from './HomeClient';
+import ComprarCartaButton from './ComprarCartaButton';
 
 export {
   Jogador,
   Navbar,
-  // Salas,
-  // SalaCard,
-  // SalaForm,
+  Salas,
+  SalaForm,
   Dealer,
   FormLogin,
   LogoutButton,
   FormCadastro,
   ListarPessoasButton,
-  Loading
+  Loading,
+  HomeClient,
+  ComprarCartaButton
 }
