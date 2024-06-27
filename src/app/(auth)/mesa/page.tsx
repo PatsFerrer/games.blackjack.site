a101a9e80db5e8dc387a7ee62ddd73e3dcc18499
Patricia Ferrer <patricia.ferreira@esx.com.br>
2024-06-27 15:22:06 -0300

    style: atualizar posição dos botões de top-10 para top-20

--- src/app/(auth)/mesa/page.tsx
+++ src/app/(auth)/mesa/page.tsx
@@ -32,11 +32,11 @@ export default async function Mesa() {
 
     <main className="flex justify-center items-center min-h-screen bg-green-800">
 
-      <Link href="/home" className="md:hidden btn bg-blue-950 text-white hover:bg-blue-900 absolute top-10 left-5"><FaArrowLeft /></Link>
-      <Link href="/home" className="hidden md:flex btn bg-blue-950 text-white hover:bg-blue-900 absolute top-10 left-5"><FaArrowLeft />Deixar a mesa</Link>
+      <Link href="/home" className="md:hidden btn bg-blue-950 text-white hover:bg-blue-900 absolute top-20 left-5"><FaArrowLeft /></Link>
+      <Link href="/home" className="hidden md:flex btn bg-blue-950 text-white hover:bg-blue-900 absolute top-20 left-5"><FaArrowLeft />Deixar a mesa</Link>
 
-      <button className="md:hidden btn bg-blue-950 text-white hover:bg-blue-900 absolute top-10 right-5"><FaShareAlt /></button>
-      <button className="hidden md:flex btn bg-blue-950 text-white hover:bg-blue-900 absolute top-10 right-5">Convidar amigos<FaShareAlt /></button>
+      <button className="md:hidden btn bg-blue-950 text-white hover:bg-blue-900 absolute top-20 right-5"><FaShareAlt /></button>
+      <button className="hidden md:flex btn bg-blue-950 text-white hover:bg-blue-900 absolute top-20 right-5">Convidar amigos<FaShareAlt /></button>
 
       {/* Mesa */}
       <div className="relative w-4/5 h-96 border border-indigo-100 rounded-3xl md:max-w-[950px] bg-blue-950 shadow-sm shadow-slate-900 hover:shadow-lg">
