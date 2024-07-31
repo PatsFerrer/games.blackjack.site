fec3053ec01ecb7b64a66ae044c803ec34006342
Patricia Ferrer <patricia.ferreira@esx.com.br>
2024-07-31 12:52:27 -0300

    refactor: Remover SVG e ajustar estilos do botão

--- src/components/SalaForm.tsx
+++ src/components/SalaForm.tsx
@@ -19,14 +19,6 @@ const SalaForm: FC<SalasProps> = ({ closeModal, onCreate }) => {
     <form action={novaSala}>
       <h3 className="font-bold text-lg mb-5 text-center">Criar Sala</h3>
       <div className="input input-bordered flex items-center gap-2 mb-2">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 16 16"
-          fill="currentColor"
-          className="w-4 h-4 opacity-70"
-        >
-          <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
-        </svg>
         <input 
           type="text" 
           className="grow" 
@@ -37,18 +29,6 @@ const SalaForm: FC<SalasProps> = ({ closeModal, onCreate }) => {
         />
       </div>
       <div className="input input-bordered flex items-center gap-2 relative">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 16 16"
-          fill="currentColor"
-          className="w-4 h-4 opacity-70"
-        >
-          <path
-            fillRule="evenodd"
-            d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5.5.5.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
-            clipRule="evenodd"
-          />
-        </svg>
         <input 
           type={showPassword ? "text" : "password"} 
           className="grow" 
@@ -66,7 +46,7 @@ const SalaForm: FC<SalasProps> = ({ closeModal, onCreate }) => {
       </div>
       <button 
         type="submit" 
-        className="btn btn-success mt-5"
+        className="btn mt-5 bg-devland hover:bg-devland-200 text-white"
         onClick={handleSubmit}
       >
         Criar
