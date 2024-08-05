94b2c5897eec8a8efba9746f0659e457595d29d1
Patricia Ferrer <patricia.ferreira@esx.com.br>
2024-08-05 16:01:17 -0300

    fix: corrigir desalinhamento dos elementos no componente ListarPessoasFichas

--- src/components/ListarPessoasFichas.tsx
+++ src/components/ListarPessoasFichas.tsx
@@ -1,6 +1,6 @@
 import { FC } from "react";
 import { ITopJogador } from "@/interface/ITopJogador";
-import validarImagem from "@/utils/imageUtil";
+import { formatarFichas, validarImagem } from '@/utils';
 
 interface ITopJogadores {
   topJogador: ITopJogador;
@@ -10,17 +10,18 @@ const ListarPessoasFichas: FC<ITopJogadores> = ({ topJogador }) => {
 
   return (
     <div className="overflow-x-auto mb-3">
-      <div className="card flex-row bg-[#F2EFF0] mr-4 rounded-sm justify-around p-3 h-24">
+      <div className="card flex-row bg-[#F2EFF0] rounded-sm justify-between p-3 h-24 items-center">
         <div className="flex items-center gap-3">
           <div className="avatar mask mask-squircle h-12 w-12 mr-4">
             <img src={validarImagem(topJogador.avatar)} alt="Avatar Jogador" />
           </div>
-          <div>{topJogador.login}</div>
+          <div className="truncate max-w-32">{topJogador.login}</div>
         </div>
 
-        <div className="flex justify-center items-center">${topJogador.totalFichas}</div>
+        <div className="flex justify-center items-center">${formatarFichas(topJogador.totalFichas)}</div>
       </div>
     </div>
+
   );
 };
 
